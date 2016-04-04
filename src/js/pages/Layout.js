0b8d1871998f4c61404373d0d3ab008d2d2a6a9b
Joshua Coquelle <joshuacoquelle@gmail.com>
2016-04-03 19:46:28 -0700

    formatting test

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -6,25 +6,25 @@ import Nav from "../components/layout/Nav";
 
 class Layout extends React.Component 
 {
-	render () {
-		let { location } = this.props;
+    render () {
+        let { location } = this.props;
 
-		return (
-			<div>
-				<Nav />
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-12">
-							<h1>KillerNews.net</h1>
-							{this.props.children}
-						</div>
-					</div>
-				</div>
+        return (
+            <div>
+                <Nav />
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>KillerNews.net</h1>
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
 
-				<Footer />
-			</div>
-		);
-	}
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Layout
